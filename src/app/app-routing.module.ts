0f907c856605dc8e455de64a33b39f5ce2da93e8
Alex Zhu <alex.t.zhu@gmail.com>
2019-08-29 17:02:19 -0700

    Skeleton Angular 8 app with routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ResumeComponent } from './resume/resume.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+    { path: '',           component: HomeComponent, pathMatch: 'full'},
+    { path: 'resume',     component: ResumeComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
