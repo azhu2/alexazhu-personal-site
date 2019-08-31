be6f928c5f2014112a548fab86fa8be3af271ec9
Alex Zhu <alex.t.zhu@gmail.com>
2019-08-30 21:45:08 -0700

    Not found page and Material setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ResumeComponent } from './resume/resume.component';
 import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-    { path: '',           component: HomeComponent, pathMatch: 'full'},
-    { path: 'resume',     component: ResumeComponent }
+    { path: '',           component: HomeComponent, pathMatch: 'full' },
+    { path: 'resume',     component: ResumeComponent },
+    { path: '**',         component: NotFoundComponent }
 ];
 
 @NgModule({
