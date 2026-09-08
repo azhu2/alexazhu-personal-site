ffa8b809335964b9f335a5c1c78eaf3968d64f25
Alex Zhu <alex@alexazhu.com>
2026-09-07 19:32:42 -0700

    Fixes after version updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,38 @@
 import { NgModule } from '@angular/core';
 
+import { provideHttpClient, withInterceptorsFromDi, withXhr, HttpClient } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClient, provideHttpClient, withInterceptorsFromDi, withXhr } from '@angular/common/http';
 
 // Material
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Markdown Renderer
-import { MarkdownModule } from 'ngx-markdown';
+import { MarkdownComponent, provideMarkdown } from 'ngx-markdown';
 
 // Components
 import { AppComponent } from './app.component';
-import { ProjectMenuComponent } from './components/project-menu/project-menu.component';
 import { LinksToolbarComponent } from './components/links-toolbar/links-toolbar.component';
+import { ProjectMenuComponent } from './components/project-menu/project-menu.component';
 
 // Directives
 import { HoverFocusDirective } from './directives/hover-focus.directive';
 
 // Pages
-import { HomeComponent } from './pages/home/home.component';
-import { ResumeComponent } from './pages/resume/resume.component';
-import { LinksComponent } from './pages/links/links.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { NewTabComponent } from './pages/new-tab/new-tab.component';
 import { CallBlockerComponent } from './pages/call-blocker/call-blocker.component';
 import { FlippingComponent } from './pages/flipping/flipping.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LinksComponent } from './pages/links/links.component';
+import { NewTabComponent } from './pages/new-tab/new-tab.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ResumeComponent } from './pages/resume/resume.component';
 
-@NgModule({ declarations: [
+@NgModule({
+    declarations: [
         AppComponent,
         ResumeComponent,
         HomeComponent,
@@ -51,14 +52,12 @@ import { FlippingComponent } from './pages/flipping/flipping.component';
         MatToolbarModule,
         MatMenuModule,
         MatTooltipModule,
-        MarkdownModule.forRoot({
+        MarkdownComponent
+    ], providers: [
+        provideHttpClient(withXhr(), withInterceptorsFromDi()),
+        provideMarkdown({
             loader: HttpClient,
-            markedOptions: {
-                provide: {},
-                useValue: {
-                    grm: true,
-                    smartypants: true
-                }
-            }
-        })], providers: [provideHttpClient(withXhr(), withInterceptorsFromDi())] })
+        })
+    ]
+})
 export class AppModule { }
